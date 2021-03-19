9705395e6a761c735c64449ac0206643eb5852da
sayed <sayedahamed714@gmail.com>
2021-03-19 14:13:36 +0600

    setup

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+(function($){
+	$(document).ready(function(){
+
+		
+
+           $('.slider-active').owlCarousel({
+
+           		items:1,
+           		loop:true,
+           		dots: true,
+           		autoplay:true,
+           		autoplaySpeed:500,
+           		nav:true,
+           		smartSpeed:1000,
+           		autoplayHoverPause:true,
+
+           });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+
+
+
+	});
+})(jQuery)
