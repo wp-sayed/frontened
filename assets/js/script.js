33afed4fec82e2a91c73b70c02e7715130b7451c
sayed <sayedahamed714@gmail.com>
2021-03-20 22:25:10 +0600

    typography button components update

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,11 +13,32 @@
            		nav:true,
            		smartSpeed:1000,
            		autoplayHoverPause:true,
+              navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+              responsive:{
+                0:{
+                  items:1
+                },
+                600:{
+                  items:1
+                },
+                1000:{
+                  items:1
+                }
+              }
 
            });
 
 
+           //sticky header
 
+           $(window).on('scroll',function(){
+             let scroll = $(window).scrollTop();
+             if(scroll < 20){
+               $('.sticky-header').removeClass('sticky');
+             }else{
+               $('.sticky-header').addClass('sticky');
+             }
+           });
 
 
 
